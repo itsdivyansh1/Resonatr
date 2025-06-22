ed83155ba30214fe4ef369f0f7dd056487f143a9
Divyansh Mishra <itsdivyansh201@gmail.com>
2025-06-22 19:48:11 +0530

    Added better auth for authentication

---
+++ src/app/(marketing)/layout.tsx
@@ -0,0 +1,9 @@
+interface MarketingLayoutProps {
+  children: React.ReactNode;
+}
+
+const MarketingLayout = ({ children }: MarketingLayoutProps) => {
+  return <>{children}</>;
+};
+
+export default MarketingLayout;
