cb208fcdd9fcef2d3d94e2fe6c5cbb0cd5740b80
Divyansh Mishra <itsdivyansh201@gmail.com>
2025-06-25 23:43:08 +0530

    fixed serverless connectivity

--- src/db/drizzle.ts
+++ src/db/drizzle.ts
@@ -2,5 +2,8 @@ import "dotenv/config";
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 
-const queryClient = postgres(process.env.DATABASE_URL!);
+const queryClient = postgres(process.env.DATABASE_URL!, {
+  prepare: false,
+  max: 1,
+});
 export const db = drizzle(queryClient);
